f10d37362c1eb574df658c6cf1e0ce02d0da0774
eduard.shvetsov <eduard.shvetsov@anvileight.com>
2021-01-18 21:37:52 +0200

    Create project structure

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AdminRoutingModule} from './admin-routing.module';
+import {AdminLayoutComponent} from './shared/admin-layout/admin-layout.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {CreatePageComponent} from './create-page/create-page.component';
+import {EditPageComponent} from './edit-page/edit-page.component';
+import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+
+@NgModule({
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    CreatePageComponent,
+    EditPageComponent,
+    DashboardPageComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ],
+  exports: [AdminRoutingModule]
+})
+export class AdminModule {
+}

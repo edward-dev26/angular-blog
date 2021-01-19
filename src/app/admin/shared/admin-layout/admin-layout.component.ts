4cdec7856ef32aa04051b29ad9642a7e6688af75
eduard.shvetsov <eduard.shvetsov@anvileight.com>
2021-01-19 18:28:20 +0200

    Add admin layout

--- src/app/admin/shared/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-layout.component.scss']
 })
 export class AdminLayoutComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
   }
 
+  logout(event: MouseEvent) {
+    event.preventDefault();
+
+    this.router.navigate(['/admin', 'login']);
+  }
 }

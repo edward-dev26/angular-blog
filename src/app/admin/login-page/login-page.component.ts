25bc8bc70567b7f2eaaab2f3b3d7bf249fbad591
eduard.shvetsov <eduard.shvetsov@anvileight.com>
2021-01-19 19:04:48 +0200

    Add login form

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
   }
 
+  isInvalid(controlName: string) {
+    return this.form.get(controlName).touched && this.form.get(controlName).invalid;
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
 }

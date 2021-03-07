e4900ad3a95544ae68554f96b9093079f79f2ea5
eduard.shvetsov <eduard.shvetsov@anvileight.com>
2021-03-07 17:33:00 +0200

    Add posts view

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Post} from '../shared/interfaces';
+import {PostsService} from '../shared/services/posts.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, OnDestroy {
+  public posts: Post[];
+  public loading = false;
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(
+    private postsService: PostsService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.loading = true;
+    const subscription = this.postsService.getAll().subscribe(posts => {
+        this.posts = posts;
+        this.loading = false;
+      },
+      () => {
+        this.loading = false;
+      }
+    );
+
+    this.subscriptions.push(subscription);
   }
 
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => {
+      subscription?.unsubscribe();
+    });
+  }
 }
--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {PostsService} from '../shared/services/posts.service';
+import {switchMap} from 'rxjs/operators';
+import {Post} from '../shared/interfaces';
+
+interface Params {
+  id: string;
+}
 
 @Component({
   selector: 'app-post-page',
@@ -6,10 +14,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-page.component.scss']
 })
 export class PostPageComponent implements OnInit {
+  public loading = false;
+  public post: Post;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService
+  ) {
   }
 
+  ngOnInit() {
+    this.loading = true;
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postService.getById(params.id);
+        })
+      )
+      .subscribe(
+        (post) => {
+          this.post = post;
+          this.loading = false;
+        },
+        () => {
+          this.loading = false;
+        });
+  }
 }
